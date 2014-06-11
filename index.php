468df24f9109c93b7bd581233a17de357e5d6218
daveyclayton <david@davidclayton.com>
2014-06-11 17:59:56 +0100

    v1

---
+++ index.php
@@ -0,0 +1,194 @@
+<?php
+preg_match('/MSIE (.*?);/', $_SERVER['HTTP_USER_AGENT'], $matches);
+
+if (count($matches)>1){
+  //Then we're using IE
+  $version = $matches[1];
+
+  switch(true){
+    case ($version<=8):
+      //IE 8 or under!
+      header('Location:browsers.html');
+      break;
+
+    case ($version==9):
+      //IE9!
+      break;
+
+    default:
+      //You get the idea
+  }
+}
+?>
+
+<!doctype html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>Perplexify is a free online tool for setting crosswords.</title>
+  <meta name="description" content="">
+
+  <meta name="viewport" content="width=device-width">
+  
+  <meta name="description" content="Perplexify lets you design beautiful, symmetrical crossword grids, analyse them and populate them with perplexing parlance">
+  <meta name="keywords" content="crosswords, more crosswords, cryptic crosswords, DIY, does anything use meta-keywords nowadays anyway?">
+  
+  <link href='http://fonts.googleapis.com/css?family=Niconne|Merienda+One|Damion|Unna|Felipa' rel='stylesheet' type='text/css'>
+  
+  <link rel="stylesheet" href="css/inputs.css">
+  <link rel="stylesheet" href="css/styles.css">
+
+</head>
+<body onresize="renderPage();" onload="renderPage();">
+<!--SHIM -->
+<div>
+ <!-- WRAP ALL -->
+ <div id="wrapall">
+ 	<div id="tag"> </div>
+ 	
+ 	
+	<!-- MAIN -->
+ 	<div id="main">
+ 
+ 
+	<div id="header"><h1>Perplexify</h1></div>
+ 
+	<div style="clear:both;"> </div>
+
+
+  	<!-- BUILD FORM -->
+  	<div id="ipform">
+  		<form id="specForm">
+  		<fieldset>
+       <legend>Build a new grid: </legend>
+        <p> <label>Grid size: </label> 
+  		<input style="width:50px;" id="sizeText" type="text" value="15"/> 
+  		</p>
+       <p>
+          <label>Symmetry: </label>           
+          <input type = "radio"
+                 name = "symmRadio"
+                 id = "symm180"
+                 value = "180"
+                 checked = "checked" />
+          <label for = "symm180">180&deg;</label> 
+          <input type = "radio"
+                 name = "symmRadio"
+                 id = "symm90"
+                 value = "90" />
+          <label for = "symm90">90&deg;</label>
+          
+ 		</p>
+ 		<p>
+		 <input type = "submit" value="Build" onclick="buildGrid(); renderPage(); return false;">
+        </p>       
+      </fieldset>     
+  		</form>
+  	</div>
+  	<!-- /BUILD FORM -->
+  	
+  	<!-- GRID WRAPPER -->
+  	<div id="gridsheet">
+  	
+  	<!-- GRID INTERFACE -->
+  	<div id="interfacewrapper">
+  	
+  	
+  	<!-- GRID TABLE ELEMENTS-->
+  	<div id="gridtablecontainer">
+  	
+  	<table id="gridtable"> </table>
+  	
+  	</div>
+  	<!-- /GRID TABLE ELEMENTS-->
+  	
+  	
+  	<!-- GRID FORM ELEMENTS-->
+  	
+  	<div id="gridform">
+  	
+  	<form id="processForm">
+  	<fieldset>
+  	<legend>Options: </legend><div>
+  	<input class="formbut" id="startover" type = "submit" value="Start Over" onclick="startOver(); renderPage(); return false;" title="Choose a new grid spec.">
+    <input class="formbut" id="addnumbers" type = "submit" value="Add numbers" onclick="addNumbers(); renderPage(); return false;" title="Show the clue numbers and word-length distribution">
+    <input class="formbut" id="usegrid" type = "submit" value="Use grid" onclick="useGrid(); renderPage(); return false;" title="Start adding your answers">
+    <input class="formbut" id="releasegrid" style="display:none; width:120px;" type = "submit" value="Release grid" onclick="theGrid.releaseGrid(); renderPage(); return false;" title="Edit the grid layout">
+       </div><ul class="instr" id="instr1"><li>Click on the squares to block/unblock.</li><li>Choose "add numbers" to show clue numbers word-length distributions.</li><li>Choose "use grid" to type your answers into the grid.</li></ul>
+       <ul class="instr" id="instr2"><li>Click on a square to type in a letter.</li><li>Choose "start over" to build a new grid</li><li>Choose "release grid" to edit your current grid layout</li></ul>
+       
+       
+       </fieldset>
+  	</form>
+  	
+  	</div>
+  	
+  	<!-- /GRID FORM ELEMENTS-->
+  	
+  	<div style="clear:both;"> </div>
+  		
+  	</div>
+  	<!-- /GRID INTERFACE -->
+  	
+  	</div>	
+	<!-- /GRID WRAPPER -->
+	
+	
+	
+<!--clues lists-->
+<div id="metalist">
+<div class="rule"> </div>
+  <div id="cluelist">
+  
+  <div id="cla">
+  	<h2>Across</h2>
+  	<ul id="acrosslist">
+  	</ul>
+  	</div>
+  	
+  	<div id="cld">
+  	<h2>Down</h2>
+  	<ul id="downlist">
+  	</ul>
+  	</div>
+  	
+  	<div id="cllen">
+  	<h2>Distribution</h2>
+  	<div id="cllistwrapper">
+  	<ol id="distlist">
+  	</ol>
+  	</div>
+  	
+  	</div>
+  	
+  	</div>	
+  	</div>
+<!--/clues lists-->
+
+  	<div style="clear:both;"> </div>
+  	
+</div>
+<!-- /MAIN -->
+
+
+<div id="footer">
+<h4>What's all this malarky then?</h4>
+<div id="footercontent">
+<div id="footerleft">
+<p>Well, since you ask... I was looking for a simple tool to create and fill crossword grids but couldn't find one, so I made my own. It's a bit shonky, so please be gentle.</p>
+</div>
+<div id="footerright">
+<p>If you'd like to share your comments or suggestions, tweet me up <a href="http://www.twitter.com/daveyclayton">@daveyclayton</a></p>
+</div>
+<div style="clear:both;"></div>
+</div>
+</div>
+</div>
+<!-- /WRAP ALL -->
+</div>
+<!--/SHIM -->
+
+  <script src="xw.js"></script>
+  
+</body>
+</html>
---
+++ xw.js
@@ -0,0 +1,621 @@
+function startOver(){
+clearClues();
+theGrid = new Object();
+document.getElementById('gridtable').innerHTML = '';
+document.getElementById('gridform').style.display = "none";
+document.getElementById('ipform').style.display = "inline-block";
+
+
+document.getElementById('addnumbers').style.display = btype;
+document.getElementById('usegrid').style.display = btype;
+document.getElementById('releasegrid').style.display = 'none';
+document.getElementById('instr1').style.display = 'block';
+document.getElementById('instr2').style.display = 'none';
+
+document.getElementById('interfacewrapper').style.display = "none";
+document.getElementById('gridtablecontainer').style.display = "none";
+document.getElementById('cluelist').style.display = "none";
+document.getElementById('interfacewrapper').style.display = "none";
+
+ return false;
+ 
+}
+
+var theGrid = new Object();
+var caught = false;
+var btype = 'block';
+
+function renderPage(){
+if(window.innerHeight<=480){
+//adjust display for buttons
+btype = 'inline-block';
+
+//alert("small page!" + window.innerHeight);
+
+}else{
+btype = 'block';
+
+//alert("big page!" + window.innerHeight);
+
+};
+
+//turn it off
+var divh = document.getElementById('footer').clientHeight;
+var pageh = document.getElementById('main').clientHeight;
+
+//if the doc height is less than the window size, stick the footer at abs bottom, otherwise make it relative
+if(pageh+divh <= window.innerHeight){
+
+document.getElementById('footer').style.position = 'absolute';
+document.getElementById('footer').style.bottom = '0';
+}else{
+document.getElementById('footer').style.position = 'relative';
+};
+
+
+}
+
+function buildGrid(){
+
+if (isNaN(document.forms['specForm'].elements['sizeText'].value || Number(document.forms['specForm'].elements['sizeText'].value)*1>40 || Number(document.forms['specForm'].elements['sizeText'].value)*1 < 3)) {
+alert('Um, your grid size has to be a number, \nideally between 3 and 40');
+return false;
+}else{
+
+
+var ipsize = document.forms['specForm'].elements['sizeText'].value;
+
+if(document.forms['specForm'].elements['symm180'].checked){
+var ipsymm = 1;
+}else{
+var ipsymm = 2;
+}
+
+theGrid = new xw(ipsize, ipsymm);
+
+
+document.getElementById('gridtablecontainer').style.display = "inline-block";
+document.getElementById('gridform').style.display = "inline-block";
+document.getElementById('ipform').style.display = "none";
+
+
+document.getElementById('instr1').style.display = 'block';
+document.getElementById('instr2').style.display = 'none';
+
+ return false;
+};
+
+
+
+
+}
+
+function xw(size, symm){
+this.size = size;
+this.symm = symm;
+this.clueListDown = [];
+this.clueListAcross = [];
+this.distList = [];
+this.isNumbered = false;
+this.drawMe();
+
+}
+
+
+xw.prototype.drawMe = function(){
+
+//loop thru width+height to make the content string of table cells
+
+var cellString = '';
+
+for(var n=0;n<this.size;n++){
+cellString = cellString + '<tr>';
+for(var j=0;j<this.size;j++){
+cellString = cellString + '<td style="background-color:#FFFFFF;" id="blk'+j+'_'+n+'"><div/>';
+cellString = cellString + '</td>';
+};
+
+cellString = cellString + '</tr>';
+};
+
+document.getElementById('gridtable').innerHTML = cellString;
+
+var minDim  = (this.size * 26) + 20;
+
+document.getElementById('gridtablecontainer').style.minWidth = minDim+'px';
+document.getElementById('gridtablecontainer').style.minHeight = minDim+'px';
+
+for(var n=0;n<this.size;n++){
+for(var j=0;j<this.size;j++){
+document.getElementById('blk'+j+'_'+n).onclick=function(){setSquare(this.id);};
+
+};
+};
+
+
+document.getElementById('interfacewrapper').style.display = "inline-block";
+
+
+
+
+}
+
+
+
+function setSquare(id){
+if(theGrid.isNumbered){
+stripNumbers();
+};
+
+var x = (String(id).substr(3,(id.len))).split("_");
+
+colourUp(id);
+
+if(x[0]==x[1] && x[0]==((theGrid.size-1)/2)){
+
+//do nothing - this is the centrepoint
+
+}else{
+//go for your life
+
+
+
+
+//find & color the diagonal counterpoint
+var altid = 'blk'+((theGrid.size-1)-x[0])+'_'+((theGrid.size-1)-x[1]);
+colourUp(altid);
+
+if(theGrid.symm==1){
+
+
+}else{
+
+//find and colour 90 & 270 degree counterpoints
+var altid = 'blk'+(x[1])+'_'+((theGrid.size-1)-x[0]);
+
+colourUp(altid);
+var altid = 'blk'+((theGrid.size-1)-x[1])+'_'+(x[0]);
+
+colourUp(altid);
+
+
+
+
+};
+};
+
+
+
+}
+
+
+function colourUp(id){
+//toggleMe(id);
+var cssnip = document.getElementById(id).style
+
+
+
+if(cssnip.backgroundColor=='rgb(0, 0, 0)'){
+
+cssnip.backgroundColor='#FFFFFF';
+}else{
+cssnip.backgroundColor='#000000'
+
+};
+}
+
+function toggleMe(id){
+
+
+if(this.myState){
+this.myState = 0;
+}else{
+this.myState = 1;
+};
+
+
+}
+
+
+
+function ordinate(){
+var errorlist = [];
+for(var n=0;n<theGrid.size;n++){
+for(var j=0;j<theGrid.size;j++){
+var el = document.getElementById('blk'+j+'_'+n);
+
+///////
+if(el.style.backgroundColor=='rgb(255, 255, 255)'){
+//if it's white, check surroundings...
+//top
+
+
+var t = document.getElementById('blk'+j+'_'+(n-1));
+var tl = document.getElementById('blk'+(j-1)+'_'+(n-1));
+var tr = document.getElementById('blk'+(j+1)+'_'+(n-1));
+
+var l = document.getElementById('blk'+(j-1)+'_'+(n));
+var r = document.getElementById('blk'+(j+1)+'_'+(n));
+
+var b = document.getElementById('blk'+j+'_'+(n+1));
+var bl = document.getElementById('blk'+(j-1)+'_'+(n+1));
+var br = document.getElementById('blk'+(j+1)+'_'+(n+1));
+
+if(checkSiblings(t, l, l, tl, t, el)>0){
+errorlist.push(1);
+};
+
+if(checkSiblings(t, r, t, tr, r, el)>0){
+errorlist.push(1);
+};
+
+if(checkSiblings(b, l, l, bl, b, el)>0){
+errorlist.push(1);
+};
+
+if(checkSiblings(b, r, r, br, b, el)>0){
+errorlist.push(1);
+};
+
+};
+/////
+
+
+};
+};
+
+if(errorlist.length > 0){
+return(false);
+
+}else{
+return(true);
+};
+
+
+}
+
+function checkSiblings(ar1, ar2, ar3, ar4, ar5, el){
+
+if(ar1 && ar2 && ar3.style.backgroundColor == 'rgb(255, 255, 255)' && ar4.style.backgroundColor == 'rgb(255, 255, 255)' && ar5.style.backgroundColor == 'rgb(255, 255, 255)'){
+//problem
+//el.style.backgroundColor = '#FF0000';
+return('1');
+};
+
+}
+
+xw.prototype.releaseGrid = function(){
+clearClues();
+stripNumbers();
+document.getElementById('addnumbers').style.display = btype;
+document.getElementById('usegrid').style.display = btype;
+document.getElementById('releasegrid').style.display = 'none';
+
+
+document.getElementById('instr1').style.display = 'block';
+document.getElementById('instr2').style.display = 'none';
+
+
+for(var n=0;n<this.size;n++){
+for(var j=0;j<this.size;j++){
+document.getElementById('blk'+j+'_'+n).onclick=function(){setSquare(this.id);};
+document.getElementById('blk'+j+'_'+n).innerHTML = '<div>';
+
+};
+};
+
+
+ return false;
+ 
+ 
+ 
+}
+
+function useGrid(){
+var check = false;
+
+if(ordinate()){
+	check=true;
+}else{
+	if(confirm("Your grid has multiple adjacent spaces.\n Use it anyway?")){
+	check=true;
+	};
+}
+
+if(check==true){
+
+numerate();
+writeClues();
+document.getElementById('addnumbers').style.display = 'none';
+document.getElementById('usegrid').style.display = 'none';
+document.getElementById('releasegrid').style.display = btype;
+
+document.getElementById('instr2').style.display = 'block';
+document.getElementById('instr1').style.display = 'none';
+
+for(var n=0;n<theGrid.size;n++){
+for(var j=0;j<theGrid.size;j++){
+var el = document.getElementById('blk'+j+'_'+n);
+
+el.onclick = null;
+if(el.style.backgroundColor=='rgb(255, 255, 255)'){
+el.childNodes[0].innerHTML = el.childNodes[0].innerHTML + '<input id="ip'+j+'_'+n+'" type="text" maxlength="1"/>';
+};
+
+};
+};
+};
+
+ return false;
+ 
+ 
+}
+
+function addNumbers(){
+var check = false;
+
+if(ordinate()){
+	check=true;
+}else{
+	if(confirm("Your grid has multiple adjacent spaces.\n Number it anyway?")){
+	check=true;
+	};
+}
+
+if(check==true){
+
+theGrid.isNumbered = true;
+numerate();
+writeClues();
+};
+
+ return false;
+}
+
+function writeClues(){
+//create the clue lists...
+
+var ipstr = '';
+for(var n=0;n<theGrid.clueListAcross.length;n++){
+
+	var a = theGrid.clueListAcross[n][0];
+	var b = theGrid.clueListAcross[n][1];
+	
+	ipstr = ipstr + '<li>'+a+': <i>('+b+')</i></li>';
+	
+};
+document.getElementById('acrosslist').innerHTML = ipstr;
+
+var ipstr = '';
+for(var n=0;n<theGrid.clueListDown.length;n++){
+	var a = theGrid.clueListDown[n][0];
+	var b = theGrid.clueListDown[n][1];
+	
+	ipstr = ipstr + '<li>'+a+': <i>('+b+')</i></li>';
+	
+};
+
+
+document.getElementById('downlist').innerHTML = ipstr;
+
+/// and write the distribution table
+
+//write distribution list
+
+var ipstr = '';
+for(var n=0;n<theGrid.size;n++){
+theGrid.distList.push([n+1,0]);
+};
+
+
+for(var n=0;n<theGrid.clueListAcross.length;n++){
+
+//add one for each length val to the second item of distList n[1]
+
+theGrid.distList[Number(theGrid.clueListAcross[n][1])-1][1]++;
+
+};
+
+for(var n=0;n<theGrid.clueListDown.length;n++){
+
+theGrid.distList[Number(theGrid.clueListDown[n][1])-1][1]++;
+
+};
+
+
+	
+	var maxval = theGrid.distList[0][1];
+	
+	for(var n=1;n<theGrid.distList.length;n++){
+	if(theGrid.distList[n][1] > maxval){
+	 maxval = theGrid.distList[n][1];
+	
+	};
+	};
+
+	
+	
+	//var multor = ;
+
+
+for(var n=0;n<theGrid.distList.length;n++){
+	var a = theGrid.distList[n][0];
+	var b = theGrid.distList[n][1];
+	
+	//ipstr = ipstr + '<li><strong>'+a+'</strong> letter words - '+b+'</li>';
+	
+	//snip off the trailing divider...
+	var brdOR=''
+	
+	if(n==theGrid.distList.length-1){
+	var brdOR = ' style="border-bottom:0;"';
+	}
+	
+	var unit = 100 / maxval;
+	
+	var wid = ((b*unit)+1);
+	
+	ipstr = ipstr + '<li'+brdOR+'><span class="clbar" style="width:'+wid+'px;"></span><span class="clnumeral">'+b+'</span></li>';
+}
+
+
+document.getElementById('distlist').innerHTML = ipstr;
+
+document.getElementById('cluelist').style.display = "inline-block";
+
+};
+
+function stripNumbers(){
+theGrid.isNumbered = false;
+
+document.getElementById('cluelist').style.display = "none";
+
+for(var n=0;n<theGrid.size;n++){
+for(var j=0;j<theGrid.size;j++){
+var el = document.getElementById('blk'+j+'_'+n);
+
+if(el.style.backgroundColor=='rgb(255, 255, 255)'){
+if(el.childNodes[0].hasChildNodes()){
+var x = el.childNodes[0].childNodes[0]
+el.childNodes[0].removeChild(x);
+};
+};
+
+};
+};
+
+}
+
+function clearClues(){
+	document.getElementById('cluelist').style.display = "none";
+}
+
+function numerate(){
+theGrid.clueListAcross = [];
+theGrid.clueListDown = [];
+theGrid.distList = [];
+/////
+var number = 1;
+for(var n=0;n<theGrid.size;n++){
+for(var j=0;j<theGrid.size;j++){
+var el = document.getElementById('blk'+j+'_'+n);
+
+///////
+if(el.style.backgroundColor=='rgb(255, 255, 255)'){
+
+
+var t = document.getElementById('blk'+j+'_'+(n-1));
+var tl = document.getElementById('blk'+(j-1)+'_'+(n-1));
+var tr = document.getElementById('blk'+(j+1)+'_'+(n-1));
+
+var l = document.getElementById('blk'+(j-1)+'_'+(n));
+var r = document.getElementById('blk'+(j+1)+'_'+(n));
+
+var b = document.getElementById('blk'+j+'_'+(n+1));
+var bl = document.getElementById('blk'+(j-1)+'_'+(n+1));
+var br = document.getElementById('blk'+(j+1)+'_'+(n+1));
+
+//if it has a right-hand and no left hand, or lower and no upper, give it a number
+hasnumber=false;
+
+if(!l){
+if(r.style.backgroundColor == 'rgb(255, 255, 255)'){
+//add number (across)
+hasnumber=true;
+
+//loop thru and check length, add to cluelist across//////////////
+var i=j+1;
+var cllen = 1;
+
+while (document.getElementById('blk'+(i)+'_'+(n)) && document.getElementById('blk'+(i)+'_'+(n)).style.backgroundColor == 'rgb(255, 255, 255)')
+  {
+  cllen++;
+  i++;
+  };
+
+var clueSet = [number,cllen];
+theGrid.clueListAcross.push(clueSet);
+////////
+
+};
+}else{
+if(r){
+if(l.style.backgroundColor != 'rgb(255, 255, 255)' && r.style.backgroundColor == 'rgb(255, 255, 255)'){
+//add number (across)
+hasnumber=true;
+//loop thru and check length, add to cluelist across//////////////
+var i=j+1;
+var cllen = 1;
+
+while (document.getElementById('blk'+(i)+'_'+(n)) && document.getElementById('blk'+(i)+'_'+(n)).style.backgroundColor == 'rgb(255, 255, 255)')
+  {
+  cllen++;
+  i++;
+  };
+
+var clueSet = [number,cllen];
+theGrid.clueListAcross.push(clueSet);
+////////
+};
+};
+};
+
+if(!t){
+if(b.style.backgroundColor == 'rgb(255, 255, 255)'){
+//add number (down)
+hasnumber=true;
+//loop thru and check length, add to cluelist across//////////////
+var i=n+1;
+var cllen = 1;
+
+while (document.getElementById('blk'+(j)+'_'+(i)) && document.getElementById('blk'+(j)+'_'+(i)).style.backgroundColor == 'rgb(255, 255, 255)')
+  {
+  cllen++;
+  i++;
+  };
+
+var clueSet = [number,cllen];
+theGrid.clueListDown.push(clueSet);
+////////
+};
+}else{
+if(b){
+if(t.style.backgroundColor != 'rgb(255, 255, 255)' && b.style.backgroundColor == 'rgb(255, 255, 255)'){
+//add number (down)
+hasnumber=true;
+//loop thru and check length, add to cluelist across//////////////
+var i=n+1;
+var cllen = 1;
+
+while (document.getElementById('blk'+(j)+'_'+(i)) && document.getElementById('blk'+(j)+'_'+(i)).style.backgroundColor == 'rgb(255, 255, 255)')
+  {
+  cllen++;
+  i++;
+  };
+
+var clueSet = [number,cllen];
+theGrid.clueListDown.push(clueSet);
+////////
+};
+};
+};
+
+if(hasnumber){
+	el.innerHTML='<div class="innards"><span>'+number+'</span></div>';
+	number++;
+}else{
+el.innerHTML='<div class="innards"></div>';
+};
+
+};
+};
+/////
+
+
+
+};
+/////
+
+
+}
+
